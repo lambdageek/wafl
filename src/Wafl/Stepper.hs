c4100a8c89fc31a602e7b87b6a1439f7a4e28636
Aleksey Kliger <aleksey@lambdageek.org>
2017-06-24 23:24:28 -0400

    Cleanup Stepper

--- src/Wafl/Stepper.hs
+++ src/Wafl/Stepper.hs
@@ -23,17 +23,38 @@ type LocalEnv = (Assoc Variable Value, (LVar, [Value]))
 type ResidualProgram = Decls
 
 data GlobalCmd = NextDecl
-               | EvalResult Value
-               | Eval FunEnv FunCmd [FunFrame]
+               | PragmaAnswer EvalRes
+               | Eval EvalStep
                deriving (Show)
 
-data FunCmd = Ret Value
-            | Jmp CVar Value [Value]
-            | Step LocalEnv Term
+data EvalStep = EvalAns FunRes [FunFrame]
+             | EvalFun FunStep [FunFrame]
+             deriving (Show)
+
+data EvalRes = EvalResult Value
+             deriving (Show)
+
+data FunStep = FunStep FunEnv LocalStep
+             | FunAns FunEnv LocalRes
+             deriving (Show)
+
+data FunRes = FunRet Value
+             | FunCall FVar Value FunFrame
             deriving (Show)
 
-data FunFrame = FunFrame FunEnv CVar [Value]
-  deriving (Show)
+data LocalStep = Step LocalEnv Term
+              | Jmp CVar Value [Value]
+            deriving (Show)
+
+data LocalRes = Ret Value
+              | LocalCall FVar Value LocalFrame
+              deriving (Show)
+
+data FunFrame = FunFrame FunEnv LocalFrame
+              deriving (Show)
+
+data LocalFrame = LocalFrame CVar [Value]
+                deriving (Show)
 
 data Result = Done
             | ErrUnboundFn FVar
@@ -56,124 +77,154 @@ step cmd_ p =
       NilDecls -> throwError Done
       ConsDecls (unrebind -> (dg, p')) -> do
         cmd <- case dg of
-          SingleDecl (PragmaDecl pg) -> enterPragma pg
+          SingleDecl (PragmaDecl pg) -> mapExceptT readonlyGlobalEnv (enterPragma pg)
           SingleDecl (SimpleDecl d) -> addDecl d >> return NextDecl
           RecDecl (RecDecls (unrec -> ds)) -> traverse_ addDecl ds >> return NextDecl
         return (cmd, p')
-    EvalResult _val ->
-      return (NextDecl, p)
-    Eval fenv cmd fs -> do
-      cmd' <- enterEval fenv cmd fs
-      return (cmd', p)
+    PragmaAnswer _result -> return (NextDecl, p)
+    Eval estep -> do
+      x <- mapExceptT readonlyGlobalEnv (stepEvalStep estep)
+      case x of
+        Left res -> return (PragmaAnswer res, p)
+        Right estep' -> return (Eval estep', p)
+
+stepEvalStep :: Fresh m => EvalStep -> ExceptT Result (ReaderT GlobalEnv m) (Either EvalRes EvalStep)
+stepEvalStep ec_ =
+  case ec_ of
+    EvalAns res fs_ ->
+      case (res, fs_) of
+        (FunRet v, []) -> return (Left $ EvalResult v)
+        (FunRet v, FunFrame fenv (LocalFrame k stk) : fs) -> do
+          return (Right $ EvalFun (FunStep fenv (Jmp k v stk)) fs)
+        (FunCall f v frame, fs) -> do
+          fcmd' <- enterFunCall f v
+          return (Right $ EvalFun fcmd' (frame:fs))
+    EvalFun fcmd fs -> do
+      x <- stepFunStep fcmd
+      case x of
+        Left res -> return (Right $ EvalAns res fs)
+        Right stp -> return (Right $ EvalFun stp fs)
+
+readonlyGlobalEnv :: Monad m => ReaderT g m a -> StateT g m a
+readonlyGlobalEnv comp = do
+  g <- get
+  lift $ runReaderT comp g
 
 addDecl :: Fresh m => Decl -> ExceptT Result (StateT GlobalEnv m) ()
 addDecl d = case d of
   FunDecl (FnDefn (unrebind -> (f, unembed -> fct))) ->
     modify ((f,fct):)
-  
 
-enterPragma :: Fresh m => Pragma -> ExceptT Result (StateT GlobalEnv m) GlobalCmd
+enterPragma :: Fresh m => Pragma -> ExceptT Result (ReaderT GlobalEnv m) GlobalCmd
 enterPragma pg =
   case pg of
     InfoPragma {} -> return NextDecl
-    EvalPragma (unembed -> (f, v)) -> enterFunction f v
+    EvalPragma (unembed -> (f, v)) -> do
+      fstp <- enterFunCall f v
+      return (Eval (EvalFun fstp []))
 
-enterFunction :: Fresh m => FVar -> Value ->
-  ExceptT Result (StateT GlobalEnv m) GlobalCmd
-enterFunction f v = do
-  m <- gets (lookup f)
-  case m of
-    Nothing -> throwError (ErrUnboundFn f)
-    Just (Function bnd) -> do
-      (kret, FnBody bodybnd) <- unbind bnd
-      (ContDefs (unrec -> conts), kenter) <- unbind bodybnd
-      let funEnv = (map mkContEnv conts, kret)
-      return $ Eval funEnv (Jmp kenter v []) []
-        
 mkContEnv :: ContDef -> (CVar, Continuation)
 mkContEnv (ContDef (unrebind -> (k, unembed -> continuation))) = (k, continuation)
 
-enterEval :: Fresh m
-          => FunEnv -> FunCmd -> [FunFrame]
-          -> ExceptT Result (StateT GlobalEnv m) GlobalCmd
-enterEval fenv cmd fs =
-  case (cmd, fs) of
-    (Ret v, []) -> return (EvalResult v)
-    _ -> do
-      g <- get
-      res <- lift $ lift $ flip runReaderT g $ runExceptT $ stepLocal fenv cmd fs
-      case res of
-        Left err -> throwError err
-        Right (fenv', cmd', fs') -> return (Eval fenv' cmd' fs')
+stepFunStep :: Fresh m
+          => FunStep
+          -> ExceptT Result (ReaderT GlobalEnv m) (Either FunRes FunStep)
+stepFunStep fcmd_ =
+  case fcmd_ of
+    FunAns fenv (LocalCall f v lf) -> do
+        let f' = FunFrame fenv lf
+        return (Left (FunCall f v f'))
+    FunAns _fenv (Ret v) -> return (Left (FunRet v))
+    FunStep fenv stp -> do
+      x <- mapExceptT (inEnvironment fenv) (stepLocal stp)
+      case x of
+        Left ans -> return (Right (FunAns fenv ans))
+        Right stp' -> return (Right (FunStep fenv stp'))
 
-
-stepLocal :: Fresh m
-  => FunEnv -> FunCmd -> [FunFrame]
-  -> ExceptT Result (ReaderT GlobalEnv m) (FunEnv, FunCmd, [FunFrame])
-stepLocal _fenv (Ret _v) [] = error "unexpected stepLocal return with empty frames"
-stepLocal _fenv (Ret v) (FunFrame fenv k stk : fs) =
-  return (fenv, Jmp k v stk, fs)
-stepLocal fenv (Jmp k v stk) fs =
-  if (snd fenv) == k
-  then do
-    case stk of
-      [] -> return (fenv, Ret v, fs)
-      _ -> throwError ErrStackNotEmpty
-  else case lookup k (fst fenv) of
-    Nothing -> throwError (ErrUnboundK k)
-    Just (Continuation bnd) -> do
-      ((x, l), m) <- unbind bnd
-      let lenv = ([(x, v)], (l, stk))
-      return (fenv, Step lenv m, fs)
-stepLocal fenv (Step lenv (Jump k v lv)) fs = do
-  stk <- evalLValue lenv lv
-  v' <- evalValue lenv v
-  return (fenv, Jmp k v' stk, fs)
-stepLocal fenv (Step lenv (Call f v lv k)) fs = do
-  stk <- evalLValue lenv lv
-  v' <- evalValue lenv v
+enterFunCall :: Fresh m
+             => FVar -> Value -> ExceptT Result (ReaderT GlobalEnv m) FunStep
+enterFunCall f v = do
   m <- asks (lookup f)
   case m of
     Nothing -> throwError (ErrUnboundFn f)
     Just (Function bnd) -> do
-      let f' = FunFrame fenv k stk
       (kret, FnBody bodybnd) <- unbind bnd
       (ContDefs (unrec -> conts), kenter) <- unbind bodybnd
       let fenv' = (map mkContEnv conts, kret)
-      return (fenv', Jmp kenter v' [], f':fs)
-stepLocal fenv (Step lenv (LetE e bnd)) fs = do
-  (x, m) <- unbind bnd
-  v <- evalExpr lenv e
-  let lenv' = addLocal x v lenv
-  return (fenv, Step lenv' m, fs)
-stepLocal _fenv _ _fs = error "finish stepLocal"
+      return (FunStep fenv' (Jmp kenter v []))
+
+stepLocal :: Fresh m
+  => LocalStep
+  -> ExceptT Result (ReaderT FunEnv m) (Either LocalRes LocalStep)
+stepLocal (Jmp k v stk) = enterJmp k v stk
+stepLocal (Step lenv m) = mapExceptT (inEnvironment lenv) (stepTerm m)
+
+enterJmp :: Fresh m
+  => CVar -> Value -> [Value]
+  -> ExceptT Result (ReaderT FunEnv m) (Either LocalRes LocalStep)
+enterJmp k v stk = do
+  (conts, kret) <- ask
+  if kret == k
+  then do
+    case stk of
+      [] -> return (Left $ Ret v)
+      _ -> throwError ErrStackNotEmpty
+  else case lookup k conts of
+    Nothing -> throwError (ErrUnboundK k)
+    Just (Continuation bnd) -> do
+      ((x, l), m) <- unbind bnd
+      let lenv = ([(x, v)], (l, stk))
+      return (Right $ Step lenv m)
 
 addLocal :: Variable -> Value -> LocalEnv -> LocalEnv
 addLocal x v (bs, lin) = ((x,v):bs, lin)
 
-evalExpr :: Monad m => LocalEnv -> Expr -> ExceptT Result m Value
-evalExpr lenv e =
+inEnvironment :: Monad m => r -> ReaderT r m a -> ReaderT r' m a
+inEnvironment lenv = withReaderT (const lenv)
+
+stepTerm :: Fresh m => Term -> ExceptT Result (ReaderT LocalEnv m) (Either LocalRes LocalStep)
+stepTerm (Jump k v lv) = do
+  stk <- evalLValue lv
+  v' <- evalValue v
+  return (Right $ Jmp k v' stk)
+stepTerm (Call f v lv k) = do
+  stk <- evalLValue lv
+  v' <- evalValue v
+  return (Left $ LocalCall f v' (LocalFrame k stk))
+stepTerm (LetE e bnd) = do
+  (x, m) <- unbind bnd
+  v <- evalExpr e
+  lenv <- ask
+  return (Right $ Step (addLocal x v lenv) m)
+stepTerm _ = error "finish stepLocal"
+
+
+evalExpr :: Monad m => Expr -> ExceptT Result (ReaderT LocalEnv m) Value
+evalExpr e =
   case e of
-    EValue v -> evalValue lenv v
+    EValue v -> evalValue v
     EOp op vs -> do
-      vs' <- traverse (evalValue lenv) vs
+      vs' <- traverse evalValue vs
       case (op, vs') of
         (Add, [IntV n1, IntV n2]) -> return (IntV $! n1 + n2)
         _ -> throwError (ErrWrongOperands op)
 
-evalValue :: Monad m => LocalEnv -> Value -> ExceptT Result m Value
-evalValue lenv v =
+evalValue :: Monad m => Value -> ExceptT Result (ReaderT LocalEnv m) Value
+evalValue v =
   case v of
     IntV {} -> return v
     UnitV -> return v
-    VarV x -> case lookup x (fst lenv) of
-      Nothing -> throwError (ErrUnboundVariable x)
-      Just v' -> return v'
+    VarV x -> do
+      m <- asks (lookup x . fst)
+      case m of
+        Nothing -> throwError (ErrUnboundVariable x)
+        Just v' -> return v'
 
-evalLValue :: Monad m => LocalEnv -> LinearValue -> ExceptT Result m [Value]
-evalLValue lenv lv =
-   case (lenv, lv) of
-     ((_vs, (l, stk)), VarLV l') ->
+evalLValue :: Monad m => LinearValue -> ExceptT Result (ReaderT LocalEnv m) [Value]
+evalLValue lv = do
+  (l, stk) <- asks snd
+  case lv of
+     VarLV l' ->
        if l == l'
        then return stk
        else throwError (ErrUnboundLV l')
